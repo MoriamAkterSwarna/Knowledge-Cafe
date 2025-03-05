d8dfcf334acb83d54024f0f475e3a487c20c6e20
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-05 20:35:39 +0600

    Code For Video 42-2 added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <>
-      <h1 className="bg-red-200 text-3xl"> Knowledge Cafe</h1>
+     <Header />
+     <Blogs />
     </>
   );
 }
