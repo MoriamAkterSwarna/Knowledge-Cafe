4baffb171f010ce610296307310246de7202ee4e
Moriam Akter Swarna <moriam15-12880@diu.edu.bd>
2025-03-06 09:31:28 +0600

    Code For video 42-4 added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,14 +1,42 @@
+const Blog = ({ blog }) => {
+  // console.log(blog);
 
+  const {
+    title,
+    cover,
+    author,
+    author_img,
+    reading_time,
+    posted_date,
+    hashtags,
+  } = blog;
 
-const Blog = ({blog}) => {
+  return (
+    <div>
+      <img src={cover} alt={`Cover Picture of the title ${title}`} />
 
-    console.log(blog);
+      <div className="flex justify-between ">
+        <div className="flex items-center">
+          <img className="w-14" src={author_img} alt="" />
 
-    return (
-        <div>
-            
+          <div className="ml-6">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
         </div>
-    );
+        <div>
+          <span>{reading_time} min read</span>
+        </div>
+      </div>
+      <h2 className="text-4xl">{title}</h2>
+
+      <p>
+        {hashtags.map((hashtag) => (
+          <span className="mr-2">#{hashtag}</span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 export default Blog;
